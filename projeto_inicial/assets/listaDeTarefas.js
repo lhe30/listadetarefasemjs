cbce9967f66dc663e7df3be4da5067da0ecda3de
Luiz <luiz.lhe30@gmail.com>
2022-07-24 14:33:21 -0300

    Projeto Lista de Tarefas finalizado

--- projeto_inicial/assets/listaDeTarefas.js
+++ projeto_inicial/assets/listaDeTarefas.js
@@ -1,60 +1,68 @@
- ( () => { 
+(() => {
     
     const criarTarefa = (evento) => {
+
     evento.preventDefault()
 
     const lista = document.querySelector('[data-list]')
-
     const input = document.querySelector('[data-form-input]')
-    const valor = input.value    
-    
+    const valor = input.value
+
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
-
-    tarefa.appendChild(Btnconclui())
+    tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
     lista.appendChild(tarefa)
-
     input.value = " "
+
 }
+
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
 
-const Btnconclui = () => {
-    const btnconclui = document.createElement('button')
+const BotaoConclui = () => { 
+    const botaoConclui = document.createElement('button')  
     
-    btnconclui.classList.add('check-button')
-    btnconclui.innerText = 'Concluir'
-    btnconclui.addEventListener('click', conluirTarefa)
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
 
-    return btnconclui
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
 
 }
 
-    function conluirTarefa(evento) {
-        const botaoConclui = evento.target      
-        const tarefaCompleta = botaoConclui.parentElement
-        tarefaCompleta.classList.toggle('done')}
 
-    
-    
-    const BotaoDeleta = () => {
-        const botaoDeleta = document.createElement('button')
 
-        botaoDeleta.innerText = 'Deletar'
-        botaoDeleta.addEventListener('click', ()=> {
-            console.log('Deletanto')
-        }) 
-        }
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
 
-        return botaoDeleta
-    }
+    const tarefaCompleta = botaoConclui.parentElement
 
-       
-)() 
+    tarefaCompleta.classList.toggle('done')
+}
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button')
+    botaoDeleta.innerText = 'Deletar'
+    botaoDeleta.addEventListener('click', deletarTarefa)
+
+    return botaoDeleta
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target
+    const tarefaCompleta = botaoDeleta.parentElement
+
+    tarefaCompleta.remove()
+
+    return botaoDeleta
+}
+
+})()
